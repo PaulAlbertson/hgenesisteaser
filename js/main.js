345551aa92d680bee1966e5be9cab78c8c7c1a95
Paul Albertson <palbertson@innoceanusa.com>
2013-11-20 15:14:44 -0800

    Final responsive commit. Switching to desktop only verson.

--- js/main.js
+++ js/main.js
@@ -10,18 +10,45 @@ requirejs.config({
 
 		/* Trying out Hammer.js for touch events https://github.com/EightMedia/hammer.js*/
 		'hammer':['libs/hammer.min'],
+		
+		/* JS Video player from Edmunds banners*/
+		'jsvideo': ['libs/video-js/video.min'],
+		
+		/* Custom build of modernizr that just detects touch and html5*/ 
+		'modernizr':['libs/modernizr.custom.06566'],
+		
+		/* IWA detection for screen resolution and touch*/
+		'iwadetection':['libs/IWADetection'],
+		
+		// Try not to load this here. Load after touch/resolution detection
+		//'raphael':['libs/raphael.min'],
+		
         
         app: '../app'
     },
     shim: {
         /* Set bootstrap dependencies (just jQuery) */
-        'bootstrap' : ['jquery']
+        //'bootstrap' : ['jquery']
     }
 });
 
-require(['jquery', 'less', 'hammer'], function($) {
+require(['jquery', 'less', 'hammer','jsvideo','modernizr','iwadetection'], function($) {
     console.log("Loaded :)");
-    //less.watch();
+    //var hyundaiVideoPlayer = _V_("hyundai-video");
+    //var currentVideoTime = hyundaiVideoPlayer.currentTime();
+
+    //alert("Pixel Ratio - "+window.devicePixelRatio+" Detect Value: "+IWADeviceDetection.prototype.detect());
+    var retinaCheck = IWADeviceDetection.prototype.detect();
+    //alert("Detect Value: "+retinaCheck);
+    if (retinaCheck == 'no-touch'){
+    	// $.getScript( "js/libs/raphael-min.js", function() {
+		  // console.log( "Non touch - Raphael Loaded" );
+		// });
+    };
+    
+    //hyundaiVideoPlayer.pause();
+
+    less.watch();
 	
 	//$('#stageShade').animate()
 	
